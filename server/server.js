85a675899168da32321439eb1e974249de1ebc30
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-11-01 09:55:16 -0700

    switch to middleware

--- server/server.js
+++ server/server.js
@@ -6,13 +6,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-})
+//Middleware
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/styles", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.css"));
-})
+
+//Endpoints
+// app.get("/", function(req,res) {
+//     res.sendFile(path.join(__dirname, "../public"));
+// })
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// })
+
+// app.get("/styles", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.css"));
+// })
 
 
 
