cae99db6771e2f1f9df0c7de48d5f6f3a450855d
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-10-31 20:38:58 -0700

    ready for employment

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+})
+
+const port = processs.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`up on ${port}`);
+});
+
